19459f362c9b8828603361f052fea6102a47aec1
leticia.silva <frhleticia@gmail.com>
2026-02-11 17:31:39 -0300

    Add unit tests for PersonagemController; implement Jutsu addition, chakra increase, character retrieval, and deletion

--- src/test/java/com/db/naruto_II/unit/PersonagemControllerTest.java
+++ src/test/java/com/db/naruto_II/unit/PersonagemControllerTest.java
@@ -1,5 +1,6 @@
 package com.db.naruto_II.unit;
 
+import com.db.naruto_II.dto.JutsuRequest;
 import com.db.naruto_II.dto.PersonagemRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,5 +39,54 @@ public class PersonagemControllerTest {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.nome").value("Naruto"))
                 .andExpect(jsonPath("$.vida").value(100));
+    }
+
+    @Test
+    void deveAdicionarJutsuAoPersonagem() throws Exception {
+        var request = new JutsuRequest("Chidori", 50, 15);
+
+        String jutsuRequest = mapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                        post("/personagens/{id}/jutsus")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jutsuRequest)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nome").value("Chidori"))
+                .andExpect(jsonPath("$.dano").value(50))
+                .andExpect(jsonPath("$.consumoDeChakra").value(15));
+    }
+
+    @Test
+    void deveAumentarChakraDoPersonagem() throws Exception {
+        int quantidadeChakra = 20;
+
+        mockMvc.perform(
+                        post("/personagens/{id}/chakra")
+                                .param("quantidadeChakra", String.valueOf(quantidadeChakra))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.chakra").value(quantidadeChakra));
+    }
+
+    @Test
+    void deveBuscarPersonagemPorId() throws Exception {
+        mockMvc.perform(
+                        get("/personagens/{id}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nome").value("Naruto"))
+                .andExpect(jsonPath("$.vida").value(100));
+    }
+
+    @Test
+    void deveDeletarPersonagem() throws Exception {
+        mockMvc.perform(
+                        post("/personagens/{id}")
+                )
+                .andExpect(status().isNoContent());
     }
 }
