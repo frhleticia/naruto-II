79ba82411536b3295bd2f594ff14fd953406ecc8
leticia.silva <frhleticia@gmail.com>
2026-02-16 15:20:22 -0300

    Remove Mockito extension from PersonagemTest and clean up imports

--- src/test/java/com/db/naruto_II/unit/PersonagemTest.java
+++ src/test/java/com/db/naruto_II/unit/PersonagemTest.java
@@ -3,12 +3,9 @@ package com.db.naruto_II.unit;
 import com.db.naruto_II.entity.NinjaDeNinjutsu;
 import com.db.naruto_II.entity.NinjaDeTaijutsu;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 public class PersonagemTest {
 
     @Test
